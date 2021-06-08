d03182c59a2a881ea21839ded754857bdb37960e
2119299531@qq.com <2119299531@qq.com>
2021-06-08 15:53:09 +0800

    下一步使用jwt

--- distributed-security-order/src/main/java/com/itheima/order/OrderServer.java
+++ distributed-security-order/src/main/java/com/itheima/order/OrderServer.java
@@ -9,6 +9,7 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 public class OrderServer {
     public static void main(String[] args) {
         SpringApplication.run(OrderServer.class,args);
+
     }
 
 }
